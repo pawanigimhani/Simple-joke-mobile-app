038ecaf98ec6342c48c9c78cabb730848c69d1e3
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-12-21 19:47:40 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MyHomePage(title: '😄 Fresh Jokes'),
+      home: const MyHomePage(title: 'Fresh Jokes'),
     );
   }
 }
